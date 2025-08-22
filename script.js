f0200b667c4e6f0c7835d42fd34355907a226471
alihps84 <ali.h.p.s84@gmail.com>
2025-08-22 13:50:21 +0330

    Update script.js

--- script.js
+++ script.js
@@ -118,7 +118,7 @@ function enableNotifications() {
 }
 
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('service-worker.js')
         .then(() => console.log('Service Worker registered'))
         .catch(err => console.error('Service Worker registration failed:', err));
 }
